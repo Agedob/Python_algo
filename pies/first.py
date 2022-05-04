af0098af4d5a819eaf77deae818ca5de8e9bb3cc
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-04 13:20:21 -0700

    break

--- pies/first.py
+++ pies/first.py
@@ -3,6 +3,9 @@ lists = [
 [1,2,5,6,2],
 [1,2,5,6,2],
 
+[1,2,5,6,3],
+[1,2,5,6,2],
+
 [1,2,5,6,5],
 [1,2,5,6,5,3],
 
@@ -13,17 +16,23 @@ lists = [
 ['celery','carrots','bread','cream']
 ]
 
-def compare (l1, l2):
-    print(l1,l2)
-
-# compare(lists[0],lists[1])
-
-
 def forloops ():
+    
     for i in range(0, len(lists), 1):
-        # print(lists[x])
-        
+        print('working on lists', lists[i], 'and ', lists[i+1])
+        if len(lists[i]) != len(lists[i+1]):
+            print("lists are not the same length")
+            i += 1
+            break
         for j in range(0, len(lists[i]), 1):
-            print(lists[i][j])
+            # print(lists[i][j])
+            if lists[i][j] != lists[i + 1][j]: 
+                print('Char ' , lists[i][j] , ' does not match ' , lists[i+1][j])
+                break
+            elif lists[i][j] != lists[i + 1][j]:
+                print(lists[i][j], lists[i + 1][j], 'Does not match')
+                break
+           
+
 
 forloops()
